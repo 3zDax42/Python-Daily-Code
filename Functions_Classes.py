1c31b391d43616526d4c4ffc67ac6d354e25df79
3zDax42 <ezri@thebandit.org>
2025-03-05 14:45:05 -0700

    Create Functions_Classes.py

---
+++ Functions_Classes.py
@@ -0,0 +1,21 @@
+# def sum_even(num):
+#     if num%2==1:
+#         num -= 1
+#     sum=0
+#     while num > 0:
+#         sum += num
+#         num -= 2
+#     return sum
+# print(sum_even(7))
+
+# def Calculate_BMI(Weight, Height):
+#     return (Weight / (Height * Height))
+# print(Calculate_BMI(14,18))
+
+# def count_down_up(n):
+#     for i in range (n, 1, -1):
+#         print(i)
+#     for i in range (1, n+1, +1):
+#         print(i)
+# count_down_up(6)
+
