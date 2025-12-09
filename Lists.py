c4b8c97dad0db73100f82e637f075f12b0d677bb
3zDax42 <ezri@thebandit.org>
2025-12-09 15:02:10 -0700

    Create Lists.py

---
+++ Lists.py
@@ -0,0 +1,9 @@
+Zoo = ["Monkey", "Elephant", "Lion", "Tiger", "Giraffe"]
+print (Zoo)
+Zoo.append("Panda")
+print (Zoo)
+print(len(Zoo))
+Zoo.remove("Lion")
+print(Zoo)
+Zoo[1] = "Meerkat"
+print(Zoo)
