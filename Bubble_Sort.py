4b77c3add0dd876f534d32defd207b14f0d3c40a
3zDax42 <ezri@thebandit.org>
2026-01-14 14:49:50 -0700

    Update Bubble_Sort.py

--- Bubble_Sort.py
+++ Bubble_Sort.py
@@ -1,10 +1,15 @@
-Number_Sort = [16,857,257,856,25,287,645,8647,345,816,73,54,15,87,65,87,94,984,561984,156874,156984516,516,456]
+import random
 
-for y in range(len(Number_Sort)-1):
-    for i in range(len(Number_Sort)-1-y):
-        if Number_Sort[i] > Number_Sort[i+1]:
-            temp = Number_Sort[i]
-            Number_Sort[i] = Number_Sort[i+1]
-            Number_Sort[i+1] = temp
-        
-    print(Number_Sort)
+RandList = [random.randrange(100) for x in range(10)]
+print(RandList)
+
+def BubbleSort(OrignList):
+    for j in range(len(OrignList)-1):
+        for i in range(len(OrignList)-j-1):
+            if OrignList[i] > OrignList[i+1]:
+                temp = OrignList[i]
+                OrignList[i] = OrignList[i+1]
+                OrignList[i+1] = temp
+    return OrignList
+
+print(BubbleSort(RandList))
